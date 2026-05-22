bf6cb29b9c0626b960f3ab4c1db4990b2407b8e4
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-05-22 23:56:30 +0900

    feat: connect mongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -17,3 +17,14 @@ dotenv.config();
 // console.log("PORT:", process.env.PORT);
 
 // console.log("mongo_url:", process.env.MONGO_URL);
+
+// CLUSTER => Database => Collection => Document
+import mongoose from "mongoose";
+
+mongoose
+    .connect(process.env.MONGO_URL as string, {})
+    .then((data) => {
+        console.log("MongoDB connection succeed");
+        const PORT = process.env.PORT ?? 3003;
+    })
+    .catch((err) => console.log("Error on Connection MongoDB", err));
