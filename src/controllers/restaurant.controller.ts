e69fb9f94862e12273a7a68e0d3f3a609e0be3ce
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-06-09 18:15:06 +0900

    fix: create sessions on login and sign up process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from '../models/Member.service';
-import { MemberInput } from "../libs/types/member";
+import { AdminRequest, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 import { LoginInput  } from "../libs/types/member";
 
@@ -21,7 +21,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
     }
 };
 
-restaurantController.processSignup = async (req: Request, res: Response) => {
+restaurantController.processSignup = async (req: AdminRequest, res: Response) => {
     try {
         console.log("processSignup");
 
@@ -31,7 +31,13 @@ restaurantController.processSignup = async (req: Request, res: Response) => {
 
         const result = await memberService.processSignup(newMember);
         // TODO: SESSIONS AUTHENTICATION
-        res.send(result);
+
+        req.session.member = result;
+        req.session.save(function() {
+            res.send(result);
+        });
+
+
     } catch(err) {
         console.log("Error, processSignup:", err);
         res.send(err);
@@ -60,7 +66,7 @@ restaurantController.getLogin = (req: Request, res: Response) => {
 
 
 
-restaurantController.processLogin = async (req: Request, res: Response) => {
+restaurantController.processLogin = async (req: AdminRequest, res: Response) => {
     try {
         console.log("processLogin");
         console.log("body:", req.body);
@@ -68,7 +74,13 @@ restaurantController.processLogin = async (req: Request, res: Response) => {
 
         const result = await memberService.processLogin(input);
         // TODO: SESSIONS AUTHENTICATION
-        res.send(result);
+
+          req.session.member = result;
+        req.session.save(function() {
+            res.send(result);
+        });
+
+
     } catch (err) {
         console.log("Error, processLogin:", err);
         res.send(err);
