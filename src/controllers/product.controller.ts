3a16c4ebcf9098fb6ce8c79dcb51ff8e55bcc422
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-06-10 00:43:34 +0900

    feat: create product controller and product routing apis

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import Errors from "../libs/Errors";
+import { T } from "../libs/types/common";
+import ProductService from "../models/Product.service";
+
+const productService = new ProductService();
+
+const productController: T = {};
+
+productController.getAllProducts = async (req: Request, res: Response) => {
+    try {
+        console.log("getAllProducts");
+        res.render("product");
+} catch (err) {
+        console.log("ERROR, getAllProducts:", err)
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+        // res.json({})
+    }
+};
+
+productController.createNewProduct = async (req: Request, res: Response) => {
+    try {
+        console.log("createNewProduct");
+} catch (err) {
+        console.log("ERROR, createNewProduct:", err)
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+        // res.json({})
+    }
+};
+
+productController.updateChosenProduct = async (req: Request, res: Response) => {
+    try {
+        console.log("updateChosenProduct");
+} catch (err) {
+        console.log("ERROR, updateChosenProduct:", err)
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+        // res.json({})
+    }
+};
+
+
+export default productController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
 // default chaqirilganda yaxlit chaqiriladi
 
 
@@ -20,6 +21,9 @@ routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /** Restaurant Product */
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct);
 
 /** Restaurant User */
 
