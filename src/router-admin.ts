4df8e3b98a62e65f410ee3e46373ce38c7a39a47
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-06-10 08:34:38 +0900

    feat: upload files into server via multer

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 // default chaqirilganda yaxlit chaqiriladi
 
 
@@ -14,7 +15,9 @@ routerAdmin
 
 routerAdmin
     .get("/signup", restaurantController.getSignup)
-    .post("/signup", restaurantController.processSignup);
+    .post("/signup", 
+        makeUploader("members").single("memberImage"), 
+        restaurantController.processSignup);
 
 routerAdmin.get("/logout", restaurantController.logout);
 
@@ -28,6 +31,8 @@ routerAdmin.get(
 );
 routerAdmin.post("/product/create", 
     restaurantController.verifyRestaurant, // middleware
+    // uploadProductImage.single('productImage'),
+    makeUploader("products").array ("productImage", 5),
     productController.createNewProduct);
 routerAdmin.post("/product/:id", 
     restaurantController.verifyRestaurant,
