00f738ea38fa909f0265bec0cd99ef3c385c73fc
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-06-19 22:45:16 +0900

    feat: signup process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,33 @@
 console.log("Signup frontend javascript file");
+
+$(function(){     });
+
+    //   $(".member-nick").click(function() {
+    //     alert(".member-phone").toggle();
+    //   });
+    // 
+function validateSignupForm() {
+        // console.log("Executed validateSignupForm");
+    const memberNick = $(".member-nick").val();
+    const memberPhone = $(".member-phone").val();
+    const memberPassword = $(".member-password").val();
+    const memberPassword = $(".confirm-password").val();
+
+    if (
+    memberNick === "" || 
+    memberPhone === ""  ||
+    memberPassword === "" ||
+    confirmPassword === "" 
+    ) {
+        alert ("Please insert all required inputs");
+        return false;
+    }
+
+    if (memberPassword !== confirmPassword) {
+        alert("Password differs, please check!")
+        return false;
+    }
+
+    
+}
+
