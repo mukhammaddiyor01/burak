872a98275fa6ae38544a2bf86c1fc50c31358a30
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-07-06 20:03:43 +0900

    fix: member.controller is fixed

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,16 +1,19 @@
 import { T } from '../libs/types/common'
-import { Request, Response } from 'express'
-import { LoginInput, Member, MemberInput } from '../libs/types/member';
+import { NextFunction, Request, Response } from 'express'
+import { ExtendedRequest, LoginInput, Member, MemberInput } from '../libs/types/member';
 import { MemberType } from '../libs/enums/member.enum';
 import MemberService from '../models/Member.service';
-import Errors from '../libs/Errors';
+import Errors, { HttpCode, Message } from '../libs/Errors';
 import { randomBytes } from 'crypto';
+import AuthService from '../models/Auth.service';
+import { AUTH_TIMER } from '../libs/config';
 
 // SPA - REACT uchun 
 
 const memberController: T = {};
 
 const memberService = new MemberService();
+const authService = new AuthService();
 
 const createMemberToken = (): string => randomBytes(48).toString('hex');
 
@@ -21,9 +24,17 @@ memberController.signup = async (req: Request, res: Response) => {
 
         const input: MemberInput = req.body,
             result: Member = await memberService.signup(input);
+
         // TODO: TOKENS AUTHENTICATION
-        
-        res.json({ member: result })
+        const token = await authService.createToken(result);
+
+        // TODO: TOKEN Cookie ga joylash
+        res.cookie("accessToken", token, {
+            maxAge: AUTH_TIMER * 3600 * 1000,
+            httpOnly: false,
+        });
+
+        res.status(HttpCode.CREATED).json({ member: result, accessToken: token });
     } catch (err) {
         console.log("ERROR, signup:", err)
         if (err instanceof Errors) res.status(err.code).json(err);
@@ -33,25 +44,99 @@ memberController.signup = async (req: Request, res: Response) => {
     
 };
 
-memberController.login = async (req: Request, res: Response) => {
+memberController.login = async (
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+) => {
     try {
         console.log("login");
         console.log("body:", req.body);
+
         const input: LoginInput = req.body,
-        result = await memberService.login(input)
-        // TODO: TOKENS AUTHENTICATION
-        
-        res.json({ member: result })
+            result = await memberService.login(input),
+
+            // TODO: TOKENS AUTHENTICATION
+            token = await authService.createToken(result);
+
+        // TODO: TOKEN Cookie ga joylash
+        res.cookie("accessToken", token, {
+            maxAge: AUTH_TIMER * 3600 * 1000,
+            httpOnly: false,
+        });
+
+        res.status(HttpCode.OK).json({ member: result, accessToken: token });
     } catch (err) {
         console.log("ERROR, login:", err);
         if (err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standard.code).json(Errors.standard);
         // res.json({});
     }
-
 };
 
+memberController.logout = (req: ExtendedRequest, res: Response) => {
+    try {
+        console.log("logout");
+        res.cookie("accessToken", null, { maxAge: 0, httpOnly: true });
+        res.status(HttpCode. OK).json({ logout: true });
+    } catch (err) {
+        console.log("ERROR, login:", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+    }
+};
 
+memberController.getMemberDetail = async (
+    req: ExtendedRequest, 
+    res: Response
+) => {
+    try {
+        console.log("getMemberDetail");
+        const result = await memberService.getMemberDetail(req.member);
+
+        res.status(HttpCode.OK).json(result);
+    } catch (err) {
+        console.log("ERROR, getMemberDetail:", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+    }
+};
+
+memberController.verifyAuth = async (
+    req: ExtendedRequest, 
+    res: Response, 
+    next: NextFunction
+) => {
+    try {
+        const token = req.cookies["accessToken"];
+        if (token) req.member = await authService.checkAuth(token);
+
+        if (!req.member) 
+            throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
+
+        next();
+    } catch (err) {
+        console.log("ERROR, verifyAuth:", err);
+        if (err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standard.code).json(Errors.standard);
+    }
+};
+
+memberController.retrieveAuth = async (
+    req: ExtendedRequest, 
+    res: Response, 
+    next: NextFunction
+) => {
+    try {
+        const token = req.cookies["accessToken"];
+        if (token) req.member = await authService.checkAuth(token);
+
+        next()
+    } catch (err) {
+        console.log("ERROR, retrieveAuth:", err);
+        next();
+    }
+};
 
 
 export default memberController;
