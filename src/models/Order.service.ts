2243b5aea007a9c3037ab9ae40bdef24b9fe943a
MUKHAMMADDIYOR IBROKHIMOV <imrbek0208@gmail.com>
2026-07-11 19:09:20 +0900

    feat: develop updateOrder rest Api

--- src/models/Order.service.ts
+++ src/models/Order.service.ts
@@ -2,20 +2,23 @@ import { shapeIntoMongooseObjectId } from "../libs/config";
 import { OrderStatus } from "../libs/enums/order.enum";
 import Errors, { HttpCode, Message } from "../libs/Errors";
 import { Member } from "../libs/types/member";
-import { Order, OrderInquiry, OrderItemInput } from "../libs/types/order";
+import { Order, OrderInquiry, OrderItemInput, OrderUpdateInput } from "../libs/types/order";
 import OrderModel from "../schema/Order.model";
 import OrderItemModel from "../schema/OrderItem.model";
 import { ObjectId } from 'mongoose';
+import MemberService from './Member.service';
 
 
 
 class OrderService {
     private readonly orderModel;
     private readonly orderItemModel;
+    private readonly memberService;
 
     constructor() {
         this.orderModel = OrderModel;
         this.orderItemModel = OrderItemModel;
+        this.memberService = new MemberService();
     }
 
     public async createOrder(
@@ -101,6 +104,34 @@ class OrderService {
         if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
         return result;
+    }
+
+    public async updateOrder(
+        member: Member, 
+        input: OrderUpdateInput
+    ): Promise<Order>{
+        const memberId = shapeIntoMongooseObjectId(member._id),
+            orderId = shapeIntoMongooseObjectId(input.orderId),
+            orderStatus = input.orderStatus;
+
+        const result = await this.orderModel.findByIdAndUpdate({
+            memberId: memberId,
+            _id: orderId,
+        },
+        { orderStatus: orderStatus },
+        { new: true }
+    )
+    .exec();
+
+    if(!result)
+        throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+    
+    // orderStatus Pause => PROCESS points= +1
+    if(orderStatus === OrderStatus.PROCESS) {
+        await this.memberService.addUserPoint(member, 1);
+    }
+
+    return result;
     }
 }
 
--- src/router.ts
+++ src/router.ts
@@ -52,4 +52,9 @@ router.get("/order/all",
     orderController.getMyOrders
 );
 
+router.post("/order/update", 
+    memberController.verifyAuth,
+    orderController.updateOrder
+);
+
 export default router;
